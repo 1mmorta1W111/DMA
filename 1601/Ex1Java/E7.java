395cbaebc96938a85b0fbe6de33315b6c5d980bc
ImmortalWill <immortal.will.first@gmail.com>
2025-01-17 17:11:33 +0300

    ex. 2-10 done

---
+++ 1601/Ex1Java/E7.java
@@ -0,0 +1,22 @@
+//Задача 7: Обратный порядок чисел
+//Условие:
+//Пользователь вводит целое число N. Программа должна вывести числа от N до 1 в обратном порядке.
+
+        package Ex1Java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class E7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите N: ");
+        int N = scanner.nextInt();
+
+        for(int i = N;i>0 ;i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
